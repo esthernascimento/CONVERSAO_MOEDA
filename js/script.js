c64ba2c3fb679b6df7b9c461d7e061f282811ff9
Esther Nascimento <esthersolzinhacarambola@gmail.com>
2025-02-22 14:27:07 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function converter(moeda) {
+
+    const valorReais = parseFloat(document.getElementById('valorReais').value);
+    const resultadoDiv = document.getElementById('resultado');
+
+    if (isNaN(valorReais) || valorReais <= 0){
+        resultadoDiv.innerText = "Por favor, digite vaalores válidos e REAIS.";
+        return;
+    }
+
+    const taxas = {
+        'USD': 0.19,
+        'EUR': 0.16
+    };
+
+    const valorConvertido = (valorReais * taxas[moeda]).toFixed(2);
+    resultadoDiv.innerText = `Valor em ${moeda === 'USD' ? 'Dólar' : 'Euro'}: ${moeda} ${valorConvertido}`;
+
+}
